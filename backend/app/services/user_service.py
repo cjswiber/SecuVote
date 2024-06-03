53862b10a5f0e98d7c2bcdbf18ac39927304f75a
Julian Navarrete <julyplottier@hotmail.com>
2024-06-03 12:50:51 -0300

    fixed showing error 500 instead of 400 in creating user
    fixed the issue that showed error 500 instead of 400 when trying to create a user with a dni already registered in the database

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -10,16 +10,28 @@ class UserService:
         try:
             existing_user = await User.find_one(User.dni==user.dni)
             if existing_user:
-                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already registered")
-            else:    
-                user_in = User(
-                    dni=user.dni,
-                    email=user.email,
-                    hashed_password=get_password(user.password),
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="User already registered"
                 )
-                await user_in.save()
-                return user_in
+
+            user_in = User(
+                dni=user.dni,
+                email=user.email,
+                hashed_password=get_password(user.password),
+            )
+            await user_in.save()
+            return user_in
+
+
+        except HTTPException as http_exc:
+            # Re-raise HTTPException to avoid capturing it as a 500 error
+            raise http_exc
+
 
         except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=str(e)
+            )
 
